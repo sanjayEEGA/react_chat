05cadc6d513478d5614dec1c4ed0b210d3e75be8
Sanjay Ega <sanjay.ega.15@gmail.com>
2023-03-04 23:48:28 +0530

    Updates

---
+++ precache-manifest.5b1194417c94083bd0039b831295259b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1923ed80e956f33e942c6e3def786cce",
+    "url": "/react_chat/index.html"
+  },
+  {
+    "revision": "7d0be9260972e08d5cc2",
+    "url": "/react_chat/static/css/main.4200a3f9.chunk.css"
+  },
+  {
+    "revision": "7d43596023f4c61b5b5d",
+    "url": "/react_chat/static/js/2.628737bf.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/react_chat/static/js/2.628737bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d0be9260972e08d5cc2",
+    "url": "/react_chat/static/js/main.c6f40a98.chunk.js"
+  },
+  {
+    "revision": "b133b3bd26e68eb6ef13",
+    "url": "/react_chat/static/js/runtime-main.a5c642b3.js"
+  },
+  {
+    "revision": "89699827ff8385feee2ad396df0891a8",
+    "url": "/react_chat/static/media/bg-img.89699827.png"
+  },
+  {
+    "revision": "b0015d0619d6f0209eab7c88d2c5fa00",
+    "url": "/react_chat/static/media/logo.b0015d06.svg"
+  }
+]);
